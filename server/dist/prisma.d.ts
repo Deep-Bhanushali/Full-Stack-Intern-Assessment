7f8925de34b906894fe8c6fb9263154579d9b838
This is Mr-DAB <161147802+Deep-Bhanushali@users.noreply.github.com>
2025-08-27 12:33:34 +0530

    Delete server directory

--- server/dist/prisma.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "../generated/prisma";
-declare const prismaClient: PrismaClient<import("../generated/prisma").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/library").DefaultArgs>;
-export default prismaClient;
-//# sourceMappingURL=prisma.d.ts.map
--- server/dist/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type Role = "ADMIN" | "USER" | "OWNER";
-export interface JwtPayload {
-    userId: number;
-    role: Role;
-}
-//# sourceMappingURL=types.d.ts.map
